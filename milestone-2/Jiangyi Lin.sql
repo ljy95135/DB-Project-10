2e1a6408bc62a5ad776dc4222fce103cc05dcadb
Jiangyi Lin <lin.jian@husky.neu.edu>
2017-11-25 23:33:42 -0500

    Jiangyi Lin.sql finish

--- milestone-2/Jiangyi Lin.sql
+++ milestone-2/Jiangyi Lin.sql
@@ -56,7 +56,7 @@ WHERE c.CID IN (
     WHERE bc.UserID = 1)
 ORDER BY c.AvgRate DESC;
 
--- compeleted
+-- completed
 SELECT c.Name, t1.Name, t2.Name
 FROM ((Course c INNER JOIN SecondaryTopic st ON c.CID = st.CID)
 INNER JOIN Topic t1 ON t1.TID = st.TID)
@@ -83,7 +83,42 @@ ORDER BY c.AvgRate DESC;
 param:
 UserID : The user you want to check
 */
-SELECT c.Name, bc.CompleTime
+SELECT c.Name, bc.CompleteTime
 FROM BuyCourse bc INNER JOIN Course c ON bc.CID = c.CID
-WHERE bc.IsComplete
-ORDER BY bc.CompleTime;
+WHERE bc.IsComplete AND bc.UserID = 1
+ORDER BY bc.CompleteTime;
+
+-- h)
+/*
+param:
+UserID : The user you want to check
+*/
+-- enroll time/complete time/cost/code
+SELECT c.Name, c.Cost, bc.Code, bc.buyTime, bc.CompleteTime
+FROM BuyCourse bc INNER JOIN Course c ON bc.CID = c.CID
+WHERE bc.IsComplete AND bc.UserID = 1
+ORDER BY bc.CompleteTime;
+
+-- total spent
+SELECT SUM(c.Cost)
+FROM BuyCourse bc INNER JOIN Course c ON bc.CID = c.CID
+WHERE bc.IsComplete AND bc.UserID = 1;
+
+-- 1 complex report
+/*
+Find all faculties who teach at least more than 100 students course
+that are not free and have rate higher than 90 points.
+*/
+SELECT u.FirstName AS FirstName, u.LastName AS LastName
+FROM User u
+WHERE u.UserID IN(
+    SELECT DISTINCT(f.UserID)
+    FROM Faculty f INNER JOIN CreateCourse cc ON f.UserID = cc.UserID
+    WHERE cc.CID IN (
+        SELECT c.CID
+        FROM BuyCourse bc INNER JOIN Course c ON bc.CID = c.CID
+        WHERE c.Cost > 0 AND c.AvgRate > 90
+        GROUP BY c.CID
+        HAVING COUNT(*)>100
+))
+ORDER BY FirstName, LastName;
