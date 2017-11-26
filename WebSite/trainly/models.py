fff4f4bf3c70bb3a2407d1e3180b785c7923f544
Jiangyi Lin <lin.jian@husky.neu.edu>
2017-11-26 18:22:52 -0500

    User sign up

--- WebSite/trainly/models.py
+++ WebSite/trainly/models.py
@@ -9,17 +9,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 class User(models.Model):
-    userid = models.AutoField(db_column='UserID', primary_key=True)  # Field name made lowercase.
+    userID = models.AutoField(db_column='UserID', primary_key=True)  # Field name made lowercase.
     email = models.CharField(db_column='Email', unique=True, max_length=100)  # Field name made lowercase.
-    firstname = models.CharField(db_column='FirstName', max_length=50)  # Field name made lowercase.
-    lastname = models.CharField(db_column='LastName', max_length=50)  # Field name made lowercase.
+    firstName = models.CharField(db_column='FirstName', max_length=50)  # Field name made lowercase.
+    lastName = models.CharField(db_column='LastName', max_length=50)  # Field name made lowercase.
     pw = models.CharField(db_column='PW', max_length=300)  # Field name made lowercase.
-    profilepict = models.CharField(db_column='ProfilePict', max_length=50)  # Field name made lowercase.
+    profilePict = models.CharField(db_column='ProfilePict', max_length=50)  # Field name made lowercase.
     country = models.CharField(db_column='Country', max_length=50)  # Field name made lowercase.
     city = models.CharField(db_column='City', max_length=50)  # Field name made lowercase.
     street = models.CharField(db_column='Street', max_length=200)  # Field name made lowercase.
-    postalcode = models.CharField(db_column='PostalCode', max_length=20)  # Field name made lowercase.
+    postalCode = models.CharField(db_column='PostalCode', max_length=20)  # Field name made lowercase.
 
     class Meta:
         managed = False
--- WebSite/trainly/views.py
+++ WebSite/trainly/views.py
@@ -1,5 +1,50 @@
+from django.db import IntegrityError
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import RequestContext
+from django.urls import reverse
+
+from .models import User
+
+from django import forms
+
+
+class UserForm(forms.Form):
+    firstName = forms.CharField(label='First Name', max_length=50)
+    lastName = forms.CharField(label='Last Name', max_length=50)
+    country = forms.CharField(label='Country', max_length=50)
+    city = forms.CharField(label='City', max_length=50)
+    street = forms.CharField(label='Street', max_length=200)
+    postalCode = forms.CharField(label='Postal Code', max_length=20)
+    pw = forms.CharField(label='Password', widget=forms.PasswordInput())
+    email = forms.EmailField(label='Email')
+
+
+def register(request):
+    if request.method == "POST":
+        uf = UserForm(request.POST)
+        if uf.is_valid():
+            user = User()
+            user.email = uf.cleaned_data['email']
+            user.firstName = uf.cleaned_data['firstName']
+            user.lastName = uf.cleaned_data['lastName']
+            user.pw = uf.cleaned_data['pw']
+            user.profilePict = "default.jpg"
+            user.country = uf.cleaned_data['country']
+            user.city = uf.cleaned_data['city']
+            user.street = uf.cleaned_data['street']
+            user.postalCode = uf.cleaned_data['postalCode']
+            try:
+                user.save()
+            except IntegrityError:
+                return HttpResponse("Email has been used.")
+            return render(request, 'trainly/index.html')
+    else:
+        uf = UserForm()
+        return render(request, 'trainly/register.html', {'uf': uf})
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'trainly/index.html')
